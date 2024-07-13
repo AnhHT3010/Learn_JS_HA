fd94fb6870d3144cf62a29419232b704cc5b344d
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-07-13 09:50:18 +0700

    dataType

---
+++ datatype.js
@@ -0,0 +1,9 @@
+let age = 17 // number: số
+let name = "Hoang Anh" // string: chuỗi kí tự
+let isMale = true // boolean: chỉ có 2 kiểu 1 là true, 2 là false, lưu ý phải ghi đúng định dạng chứ thường không viết hoa
+let address // undefined: không xác định
+let phone = null // null: không có giá trị
+console.log(typeof phone)
+console.log(phone)
+
+// đối với dữ liệu phức tạp hơn như mảng, object, function, ... 
