c0961d772188dc2f25853c85b41f8c7306a36be1
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-07-13 09:21:35 +0700

    feature/operator

---
+++ comment&builtin.js
@@ -0,0 +1,30 @@
+// let
+// var
+// const không thay đổi trong quá trình thực thi code
+// Cách comment trong JS
+// ý nghĩa là comment sẽ không được thực thi trong quá trình chạy code
+// Cách 1: // comment
+// Cách 2: /* comment */
+
+// muốn comment 1 dòng, nhiều dòng thì Ctrl + /
+
+// Một số hàm built-in trong JS
+// alert('Hello world');
+// prompt('Nhập tên của bạn');
+// console.warn('Cảnh báo');
+// console.log("ahihi")
+
+// setTimeout
+// setTimeout(function(){
+//     let name = prompt('Nhập tên của bạn')
+//     console.log(name)
+//     alert('Xin chào ' + name)
+//     // thực hiện code sau thời gian quy định
+// }, 3000) // 3000ms = 3s
+
+// setInterval
+// setInterval(function(){
+//     // Thực hiện đoạn code lặp lại sau 1 khoảng thời gian quy định
+//     console.log('Hello')
+// }, 5000) // 1000ms = 1s
+
---
+++ opertor.js
@@ -0,0 +1,43 @@
+// Giới thiệu về toán tử trong JS
+// 1. Toán tử số học
+// 2. Toán tử gán
+// 3. Toán tử so sánh
+// 4. Toán tử logic
+
+// 1. Toán tử số học
+// Các phép + - * / ** %
+// let co_so = 3
+// let mu = 2
+// let kq = co_so ** mu
+// console.log(kq)
+
+// 2. Toán tử gán
+// let a = 5
+// let a = 2 + 4 + 5
+// console.log(a)
+
+// 3. Toán tử so sánh > < >= <= == !=
+// let a = 6
+// let b = 8
+// if(a < b){
+//     console.log('a nhỏ hơn b')
+// }
+
+// 4. Toán tử logic: 1 là đúng 2 sai
+// &&: và
+// ||: hoặc
+// !: phủ định
+
+// Ví dụ:
+// let a = 4
+// let b = 8
+// if(!(a < b || a > 5)){
+//     console.log('a nhỏ hơn b và lớn hơn 5')
+// }
+
+// Bài tập
+// Viết chương trình kiểm tra số chẵn lẻ, cho phép người dùng nhập số, nếu chẵn alert('Số chẵn'), nếu lẻ alert('Số lẻ')
+
+// Tính diện tích hình tròn, cho phép người dùng nhập bán kính, sau đó alert('Diện tích hình tròn là: ' + dien_tich)
+
+// Tính chỉ số BMI, cho phép người dùng nhập cân nặng (weight) và chiều cao (height), sau đó alert('Chỉ số BMI của bạn là: ' + bmi)
