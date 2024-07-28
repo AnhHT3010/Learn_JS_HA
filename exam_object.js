bb5b3b40fe4d5dacd0b6dc5c0186d8861efea7ee
MaiHoangAnh190907 <maihoanganhh19092007@gmail.com>
2024-07-28 08:52:16 -0400

    ádasdasdhasdhui

--- exam_object.js
+++ exam_object.js
@@ -11,3 +11,15 @@
 // In ra tên của sinh viên.
 // In ra thành phố của sinh viên (trong đối tượng address).
 // In ra tuổi của sinh viên sau 5 năm.
+
+const person = {
+    fullname : "Alice",
+    age : "22",
+    address : {
+        street : "123 Main St",
+        city : " Los Angeles",
+        zip : "90001"
+    }
+ }
+
+ console.log(`${person.fullname}  ${person.address.city}  ${Number(person.age) +5}` )
