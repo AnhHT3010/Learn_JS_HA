e8a2915ac69c823f9c42fbd445a0e161c89f2ecc
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-08-03 09:58:20 +0700

    exam

---
+++ exam/exam_loop.js
@@ -0,0 +1,6 @@
+// Bài tập 1: Bắt người dùng nhập 1 số cho đến khi nào số đó chia hết cho 5.
+
+// Bài tập 2: The FizzBuzz Job Interview Question
+// Đề bài: Viết một chương trình in các số từ 1 đến 100. Nhưng với các số chia hết cho 3,
+// in "Fizz" thay vì số đó, và với các số chia hết cho 5, in "Buzz". Với các số vừa chia hết cho 3 và 5, in "FizzBuzz".
+// Gợi ý: Sử dụng vòng lặp và các điều kiện kiểm tra.
--- loop.js
+++ loop.js
@@ -4,15 +4,24 @@
 //     console.log ( `${interger} nhân ${i} = ${ket_qua}`  )
 // }
 
-interger = Number(prompt("Nhập số mày muốn kiểm tra"));
-check = true;
-for (let i = 2; i < interger; i++) {
-  if (interger % i == 0) {
-    check = false;
-  }
-}
-if (check == true && interger > 1) {
-  console.log(`${interger} là số nguyên tố`);
-} else {
-  console.log(`${interger} không phải là số nguyên tố`);
+// interger = Number(prompt("Nhập số mày muốn kiểm tra"));
+// check = true;
+// for (let i = 2; i < interger; i++) {
+//   if (interger % i == 0) {
+//     check = false;
+//   }
+// }
+// if (check == true && interger > 1) {
+//   console.log(`${interger} là số nguyên tố`);
+// } else {
+//   console.log(`${interger} không phải là số nguyên tố`);
+// }
+
+// Đối với for: vòng lặp hữu hạn, biết trước số lần lặp
+// Đối với while: vòng lặp không biết trước số lần lặp, vòng lặp vô hạn
+
+number = Number(prompt("Nhập số muốn kiểm tra"));
+while (number < 10) {
+  console.log("Chạy lại mã này vì số ban đầu nhỏ hơn 10");
+  number = Number(prompt("Nhập số muốn kiểm tra"));
 }
