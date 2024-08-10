974cc3cdbd1c571e85dc8670dc999b82f8d3be33
MaiHoangAnh190907 <maihoanganhh19092007@gmail.com>
2024-08-09 22:39:55 -0400

    siu

--- exam/exam_array.js
+++ exam/exam_array.js
@@ -1,8 +1,28 @@
 // B1: Tính tổng các số lớn hơn hoặc bằng 0 trong mảng
-const number = [10, 1, 9, -1, -3, 5];
+// const number = [10, 1, 9, -1, -3, 5];
+// let tong = 0;
+// for (i = 0; i < number.length; i++) {
+//   if (number[i] > 0) {
+//     tong = tong + number[i];
+//   }
+// }
+// console.log(tong);
 
-// B2: Tìm số lần xuất hiện của phần tử số 10 trong mảng
-const number2 = [10, -12, 0, 1, 3, 10, 10, 10, 10, 10, 10, 101, 10, 10, 19];
-
-// B3: Tìm số lớn nhất trong mảng
-const number3 = [10, -12, 0, 1, 3, 4, 1, 23, 101, 100, 19];
+// // B2: Tìm số lần xuất hiện của phần tử số 10 trong mảng
+// const number = [10, -12, 0, 1, 3, 10, 10, 10, 10, 10, 10, 101, 10, 10, 19];
+// let count = 0;
+// for (i = 0; i < number.length; i++) {
+//   if (number[i] == 10) {
+//     count = count + 1;
+//   }
+// }
+// console.log(count);
+// // B3: Tìm số lớn nhất trong mảng
+// const number3 = [10, -12, 0, 1, 3, 4, 1, 23, 101, 100, 19];
+// Max = -999999999999999999999999;
+// for (i = 0; i < number3.length; i++) {
+//   if (number3[i] > Max) {
+//     Max = number3[i];
+//   }
+// }
+// console.log(Max);
