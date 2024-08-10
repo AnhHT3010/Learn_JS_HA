a2b57ca02049318d14e1d4ed3bd4d44de3efb4c0
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-08-10 09:17:33 +0700

    exam-array

--- array.js
+++ array.js
@@ -1,19 +1,19 @@
-const products = [
-  1,
-  "Banana",
-  "Orange",
-  "Strawberry",
-  6,
-  1234,
-  {
-    name: "Hoang Anh",
-    age: 20,
-  },
-  null,
-  false,
-  true,
-  [1, 2, 3, 4, 5, [123, 12, 1233]],
-];
+// const products = [
+//   1,
+//   "Banana",
+//   "Orange",
+//   "Strawberry",
+//   6,
+//   1234,
+//   {
+//     name: "Hoang Anh",
+//     age: 20,
+//   },
+//   null,
+//   false,
+//   true,
+//   [1, 2, 3, 4, 5, [123, 12, 1233]],
+// ];
 const navigation = [
   "Tin Tức",
   "Bóng đá",
@@ -23,5 +23,28 @@ const navigation = [
   "Sức khỏe",
   "Hi tech",
   "Thế giới",
-  "Thể thaos",
+  "Thể thao",
+  "Pháp luật",
+  "🗑️🗑️💀",
 ];
+// độ dài của mảng
+// console.log(navigation.length);
+// lấy phần tử cuối cùng
+// console.log("navigation", navigation[navigation.length - 1]);
+// cập nhật dữ liệu
+navigation[2] = "Ngoại Hạng Em 🥲";
+// thêm phần tử vào cuối mảng
+navigation.push("🫥🫥");
+navigation.push("Thời sự");
+// xóa phần tử cuối cùng
+navigation.pop();
+// console.log("navigation", navigation);
+
+// vòng lặp qua mảng
+// for (let i = 0; i < navigation.length - 1; i++) {
+//   console.log("navigation", navigation[i]);
+// }
+let number = [10, -12, 0, 1, 3];
+for (let i = 0; i <= number.length - 1; i++) {
+  if (number[i] == 0) console.log("number", number[i]);
+}
---
+++ exam/exam_array.js
@@ -0,0 +1,8 @@
+// B1: Tính tổng các số lớn hơn hoặc bằng 0 trong mảng
+const number = [10, 1, 9, -1, -3, 5];
+
+// B2: Tìm số lần xuất hiện của phần tử số 10 trong mảng
+const number2 = [10, -12, 0, 1, 3, 10, 10, 10, 10, 10, 10, 101, 10, 10, 19];
+
+// B3: Tìm số lớn nhất trong mảng
+const number3 = [10, -12, 0, 1, 3, 4, 1, 23, 101, 100, 19];
