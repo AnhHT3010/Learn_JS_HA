e0d0b6df84a2a1ebce76fba4dd2754351a2b3894
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-09-22 20:48:35 +0700

    feature/search

--- form/data-university.js
+++ form/data-university.js
@@ -6,10 +6,70 @@ let universitys = [
     document: 80,
   },
   {
-    logo: "	https://snapstudy.vn/store/1/icons/NEU.jpg",
+    logo: "https://snapstudy.vn/store/1/icons/NEU.jpg",
     title: "Đại Học Kinh Tế Quốc Dân",
     shortName: "NEU",
     document: 125,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Logo_ĐHQGHN.png",
+    title: "Đại Học Quốc Gia Hà Nội",
+    shortName: "VNU",
+    document: 95,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/vi/thumb/d/df/Logo_Đại_học_Hà_Nội.png/1200px-Logo_Đại_học_Hà_Nội.png",
+    title: "Đại Học Hà Nội",
+    shortName: "HANU",
+    document: 110,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/vi/thumb/e/e0/Logo_Học_viện_Ngoại_giao.png/200px-Logo_Học_viện_Ngoại_giao.png",
+    title: "Học Viện Ngoại Giao",
+    shortName: "DAV",
+    document: 90,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Logo_Đại_học_Giao_thông_Vận_tải.svg/1200px-Logo_Đại_học_Giao_thông_Vận_tải.svg.png",
+    title: "Đại Học Giao Thông Vận Tải",
+    shortName: "UTC",
+    document: 85,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Logo_Học_viện_Báo_chí_tuyên_truyền.png/1200px-Logo_Học_viện_Báo_chí_tuyên_truyền.png",
+    title: "Học Viện Báo Chí và Tuyên Truyền",
+    shortName: "AJC",
+    document: 70,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/vi/thumb/0/05/Logo_Học_viện_Tài_chính.png/200px-Logo_Học_viện_Tài_chính.png",
+    title: "Học Viện Tài Chính",
+    shortName: "AOF",
+    document: 78,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/8/89/Logo_Học_viện_Ngân_hàng.png",
+    title: "Học Viện Ngân Hàng",
+    shortName: "BA",
+    document: 92,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Logo_trường_Đại_học_Thủy_lợi.png/1200px-Logo_trường_Đại_học_Thủy_lợi.png",
+    title: "Đại Học Thủy Lợi",
+    shortName: "TLU",
+    document: 87,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/vi/f/ff/Logo_trường_Đại_học_Sư_phạm_Hà_Nội.jpg",
+    title: "Đại Học Sư Phạm Hà Nội",
+    shortName: "HNUE",
+    document: 102,
+  },
+  {
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Logo_Đại_học_Khoa_học_Tự_nhiên.png/800px-Logo_Đại_học_Khoa_học_Tự_nhiên.png",
+    title: "Đại Học Khoa Học Tự Nhiên",
+    shortName: "HUS",
+    document: 65,
   },
 ];
 
--- form/university.js
+++ form/university.js
@@ -2,27 +2,29 @@ import universitys from "./data-university.js";
 
 const displayUniversity = (data) => {
   const listUniversity = document.getElementById("list-university");
-  listUniversity.innerHTML = ""
+  listUniversity.innerHTML = "";
   for (let i = 0; i < data.length; i++) {
     console.log(data[i], i);
     listUniversity.innerHTML += `
       <div class="university">
-        <div class="intro">
-          <div class="wrap-img">
-            <img
-              src="${data[i].logo}"
-              alt=""
-            />
+        <a href="subjects.html">
+          <div class="intro">
+            <div class="wrap-img">
+              <img
+                src="${data[i].logo}"
+                alt=""
+              />
+            </div>
+            <div>
+              <b>${data[i].title}</b>
+              <div class="shortName">${data[i].shortName}</div>
+            </div>
           </div>
-          <div>
-            <b>${data[i].title}</b>
-            <div class="shortName">${data[i].shortName}</div>
+          <div class="description">
+            <p class="document">🗒️ ${data[i].document} Outline</p>
+            <button>See more</button>
           </div>
-        </div>
-        <div class="description">
-          <p class="document">🗒️ ${data[i].document} Outline</p>
-          <button>See more</button>
-        </div>
+        </a>
       </div>
     `;
   }
@@ -36,20 +38,20 @@ displayUniversity(universitys);
 //   Yêu cầu: in ra các phần tử ở mảng universitys có title chứa giá trị của input search-university
 // })
 
-let searching = document.getElementById("search-university")
-searching.addEventListener("change", function(){
-  console.log(searching.value)
-  let new_universities = []
-  for(let i = 0 ; i < universitys.length ; i++) {
-    let search = searching.value.toLowerCase()
-    let key = universitys[i].title.toLowerCase()
-    if ( key.includes(search) ) {
-      console.log(universitys[i])
-      new_universities.push(universitys[i])
+let searching = document.getElementById("search-university");
+searching.addEventListener("change", function (e) {
+  console.log(searching.value);
+  let new_universities = [];
+  for (let i = 0; i < universitys.length; i++) {
+    let search = searching.value.toLowerCase();
+    let key = universitys[i].title.toLowerCase();
+    let keyShort = universitys[i].shortName.toLowerCase();
+    if (key.includes(search) || keyShort.includes(search)) {
+      console.log(universitys[i]);
+      new_universities.push(universitys[i]);
     }
   }
-  displayUniversity(new_universities)
-}) 
-
+  displayUniversity(new_universities);
+});
 
 // khi thanh tra thay đổi thì cái web cũng sẽ thay đổi theo
